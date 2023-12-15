3c3363fcfa2d3bef9c0162408dbc97d489bfa2c9
mpluisacarolina <mpluisacarolina@gmail.com>
2023-12-15 14:28:51 -0400

    Contador que activa un efecto

--- src/Components/PruebasComponent.jsx
+++ src/Components/PruebasComponent.jsx
@@ -4,6 +4,8 @@ export const PruebasComponent = () => {
 
   const [usuario, setUsuario] = useState("Luisa Carolina");
   const [fecha, setFecha] = useState ("19/09/1986");
+  const [contador, setContador] = useState (0);
+  const [contador2, setContador2] = useState (0);
 
   const modUsuario = e => {
     setUsuario (e.target.value);
@@ -18,17 +20,24 @@ export const PruebasComponent = () => {
     console.log("Has cargado el componente PruebasComponent");
   }, []);
 
-    //Se ejecuta cuando cambio el usuario
+  //Se ejecuta cuando cambio el usuario
   useEffect(() => {
-    console.log("Has modificado el usuario");
+    setContador(contador+1)
+    console.log("Has modificado el usuario: " + contador);
   }, [usuario]);
+
+  // Se ejecuta cuando hay cambio de fecha
+  useEffect(() => {
+    setContador2(contador2+1)
+    console.log("Has modificado la fecha: " + contador2 + " veces");
+  }, [fecha]);
 
   return (
     <div>
         <h1> El Efecto - Hook useEffect </h1>
         
-        <strong className = 'label' > {usuario} </strong>
-        <strong> { fecha } </strong>
+        <strong className = { contador >= 10 ? 'label label-green' : 'label'} > {usuario} </strong>
+        <strong className = { contador2 >= 10 ? 'label label-green' : 'label'} > { fecha } </strong>
         <p>
           <input type="text"
             onChange = { modUsuario }
